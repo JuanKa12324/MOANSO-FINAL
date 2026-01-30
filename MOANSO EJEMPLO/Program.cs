e6080e587c0187a1bd8e30ca33baadd49792c540
Juan Alfredo Cerna Velasquez <juan.leo3107@gmail.com>
2026-01-30 03:15:14 -0500

    Creacion del menu

---
+++ MOANSO EJEMPLO/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("CALCULADORA BÁSICA");
+        Console.WriteLine("1. Sumar");
+        Console.WriteLine("2. Restar");
+        Console.Write("Elige una opción: ");
+
+        int opcion = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingresa el primer número: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingresa el segundo número: ");
+        int num2 = int.Parse(Console.ReadLine());
+    }    
+}

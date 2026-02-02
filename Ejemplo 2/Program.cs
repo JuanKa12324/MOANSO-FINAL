d2d72ef63565d2e6ebc7d4fcd87d374d7daceb47
Juan Alfredo Cerna Velasquez <juan.leo3107@gmail.com>
2026-02-02 01:52:42 -0500

    Creo menu del segundo ejemplo

---
+++ Ejemplo 2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int numeroSecreto = random.Next(1, 101); // número entre 1 y 100
+        int intento = 0;
+        int intentos = 0;
+
+        Console.WriteLine(" Adivina el número");
+        Console.WriteLine("Estoy pensando en un número entre 1 y 100");
+    }
+}
